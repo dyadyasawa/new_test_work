e57f35347676e0b4e809175fc870f947111d3708
dyadyasawa <dyadyasawa@gmail.com>
2024-08-29 07:20:58 +0300

    Правки в моделях.

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,7 @@
 
 from django.db import models
 
+from posts.models import Post
 from users.models import User
 
 NULLABLE = {"blank": True, "null": True}
@@ -12,6 +13,8 @@ class Comment(models.Model):
     content = models.TextField(verbose_name='Контент')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateField(auto_now_add=True, verbose_name='Дата редактирования')
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="Пост", **NULLABLE)
 
     def __str__(self):
         return f'{self.author}'
